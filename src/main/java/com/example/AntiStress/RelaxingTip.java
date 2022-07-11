2425fa0ea7ed97d238f3ac7d36554cee5fac398c
Andjela Rajic <andjelarajic@Andjelas-MacBook-Pro.local>
2022-07-11 22:08:09 +0200

    added empty list

--- src/main/java/com/example/AntiStress/RelaxingTip.java
+++ src/main/java/com/example/AntiStress/RelaxingTip.java
@@ -1,21 +1,19 @@
 package com.example.AntiStress;
 public class RelaxingTip {
-    private String title;
+
+    private static String title;
     private String text;
 
     public RelaxingTip(String title, String text) {
         this.title = title;
         this.text = text;
     }
-
-    public String getTitle() {
-        return title;
-    }
-
     public void setTitle(String title) {
         this.title = title;
     }
-
+    public String getTitle() {
+        return title;
+    }
     public String getText() {
         return text;
     }
